4127e7f3f37843fbd02ca6c06506bdfb387140b0
Andreas Nordahl <andnorda@gmail.com>
2025-01-16 09:12:06 +0100

    pipe stdout

--- playwright.config.ts
+++ playwright.config.ts
@@ -25,5 +25,6 @@ export default defineConfig({
         url: "http://127.0.0.1:8080",
         timeout: 120 * 1000,
         reuseExistingServer: !process.env.CI,
+        stdout: "pipe",
       },
 });
