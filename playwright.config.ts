3ba7a44dd62dc933650ff776064f74698879e88f
Andreas Nordahl <andnorda@gmail.com>
2025-01-16 09:06:04 +0100

    set playwright webServer timeout

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,21 +1,11 @@
-// playwright.config.ts
 import { defineConfig, devices } from "@playwright/test";
 
-// We need to make the base URL configurable so we can
-// change it when running tests in a Docker container.
-const BASE_URL = process.env.TEST_BASE_URL || "http://localhost:6006";
-
 export default defineConfig({
-  // ...
-  // Using the `html` reporter for visual diffing.
   reporter: process.env.CI ? "html" : "dot",
-  // ...
+
   use: {
-    baseURL: "http://127.0.0.1:8080",
-    // ...
+    baseURL: "http://localhost:8080",
   },
-  // I recommend to run regression tests at
-  // least for desktop and mobile devices.
   projects: [
     {
       name: "desktop",
@@ -28,12 +18,12 @@ export default defineConfig({
       },
     },
   ],
-  // Run your local dev server before starting the tests
   webServer: process.env.CI
     ? undefined
     : {
         command: "npx http-server ./storybook-static",
         url: "http://127.0.0.1:8080",
+        timeout: 120 * 1000,
         reuseExistingServer: !process.env.CI,
       },
 });
